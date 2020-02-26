1c09f7545ef31a8c3eb9fb5e5dc1c8387dd0490a
Vladislad T <v.trundenko@everad.com>
2020-02-26 12:10:53 +0200

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,24 @@
-import './helpers/postDate';
-import scrollSmooth from './helpers/smooth-scroll.js';
+import "./helpers/postDate";
+import scrollSmooth from "./helpers/smooth-scroll.js";
 // import example from './modules/example.js';
-import menu from './modules/menu.js';
-import reviews from './modules/reviews.js';
-import clients from './modules/clients.js';
-import footer from './modules/footer.js';
-import stat from './modules/stat.js';
-import wPopup from './modules/wPopup.js';
 
-function main () {
+import menu from "./modules/menu.js";
+import reviews from "./modules/reviews.js";
+import people from "./modules/people.js";
+import clients from "./modules/clients.js";
+import footer from "./modules/footer.js";
+import stat from "./modules/stat.js";
+import wPopup from "./modules/wPopup.js";
+
+function main() {
   scrollSmooth();
-	menu();
-	reviews();
-	clients();
-	footer();
-	stat();
-	wPopup();
+  menu();
+ 	reviews();
+	people();
+  clients();
+  footer();
+  stat();
+  wPopup();
 }
 
 main();
