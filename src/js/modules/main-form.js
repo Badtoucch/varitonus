e3a5a92586ed90f233d1e2260086db7e9e323760
Badtoucch <Badtoucch>
2020-04-09 14:47:07 +0300

    vlad

--- src/js/modules/main-form.js
+++ src/js/modules/main-form.js
@@ -1,40 +1,35 @@
 
 
 	export default () => {
+		function formTs() {
+			var people = $('.online-people');
+		var sold = $('.online-sold');
+		
 		var dynamic = $('.online-dynamic');
-		dynamic.html(randomInteger(27, 47));
 		
 		function randomInteger(min, max) {
 			var rand = min + Math.random() * (max + 1 - min);
 			return Math.floor(rand);
 		}
-		function formTs() {
-			var people = $('.online-people');
-		var sold = $('.online-sold');
-		
-	
-		
+		setInterval(function() {
+			dynamic.text(randomInteger(27, 47));
+		},8400);
 		let counter = 1;
 		setInterval(function() {
 			if (counter == 1){
 				people.fadeIn();
 				sold.fadeOut();
 				counter = 0;
+				
+
 			} else {
 				people.fadeOut();
 				sold.fadeIn();
 				counter = 1;
 			}
 		}, 4000);
+
 		
-	
-	 setInterval(function () {
-		setTimeout(function () {
-			dynamic.html(randomInteger(27, 47));
-	}, 2000);
-			}, 4000);
 		}
-		setTimeout(formTs, 2000);
-		
-	
+		setTimeout(formTs, 4000);
 	};
